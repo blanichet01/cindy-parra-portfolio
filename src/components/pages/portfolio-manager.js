a8f06d76079854443b7859d90a342b3db06f4d5c
blanicheto1 <nanililo22@gmail>
2024-08-09 17:40:35 +0200

    portfolio manager

---
+++ src/components/pages/portfolio-manager.js
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import React, { Component } from 'react';
+import PortfolioSidebarList from '../portfolio/portfolio-sidebar-list';
+import PortfolioForm from '../portfolio/portfolio-form';
+
+export default class PortfolioManager extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            portfolioItems: []
+        };
+
+        this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+        this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
+    }
+
+    handleSuccessfulFormSubmission(portfolioItem){
+       //TODO
+       //update the portfolioItems
+       //and add the new items
+    }
+
+    handleFormSubmissionError(error){
+        console.log('handleFormSubmissionError error', error);
+        
+    }
+
+    getPortfolioItems(){
+        axios
+        .get("https://cindyblanichet.devcamp.space/portfolio/portfolio_items",{
+            withCredentials: true
+        })
+        .then(response => {
+          this.setState({
+            portfolioItems: [...response.data.portfolio_items]
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+
+    componentDidMount() {
+        this.getPortfolioItems();
+      }
+    render() {
+        return (
+            <div className='portfolio-manager-wrapper'>
+                <div className='left-column'>
+                <PortfolioForm
+                 handleSuccessfulFormSubmission = {this.handleSuccessfulFormSubmission}
+                 handleFormSubmissionError = {this.handleFormSubmissionError}
+                />
+                </div>
+                <div className='righ-column'>
+                <PortfolioSidebarList data={this.state.portfolioItems}/>
+                
+                </div>
+            </div>
+        );
+    }
+}
+
+

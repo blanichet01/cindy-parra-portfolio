d77cceac48aa6dd5c83b69dd842f522dd909651d
blanicheto1 <nanililo22@gmail>
2024-08-27 20:52:07 +0200

    infinite scroll

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 import BlogItem from '../blog/blog-item';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Blog extends Component  {
     constructor(){
         super();
 
         this.state = {
-            blogItems: []
+            blogItems: [],
+            totalCount: 0,
+            currentPage:0,
+            isLoading: true
         }
 
         this.getBlogItems =this.getBlogItems.bind(this);
+        this.activateInfiniteScroll();
+    }
+
+    activateInfiniteScroll() {
+        window.onscroll = () => {
+           
+        if (window.innerHeight + document.documentElement.scrollTop == document.documentElement.offsetHeight) {
+          console.log('get more post');
+          
+        }
+        };
     }
 
     getBlogItems(){
+        this.setState({
+            currentPage:this.state.currentPage +1
+        });
+
         axios.get("https://cindyblanichet.devcamp.space/portfolio/portfolio_blogs", 
         { withCredentials:true }
         ).then(response => {
             this.setState({
-                blogItems: response.data.portfolio_blogs
+                blogItems: response.data.portfolio_blogs,
+                totalCount: response.data.meta.total_records,
+                isLoading: false
             })   
         }).catch(error => {
             console.log('getBlogItems error', error);   
@@ -35,10 +55,15 @@ class Blog extends Component  {
             return <BlogItem key={blogItem.id} blogItem={blogItem}> </BlogItem>;
         });
     return (
-        <div>
-            {blogRecords}
+        <div className='blog-container'>
+             <div className='content-container'>
+                {blogRecords}
+             </div>
+             {this.state.isLoading ? (
+             <div className='content-loader'>
+                <FontAwesomeIcon icon="spinner" spin/>
+            </div>) :null}
         </div>
-
     );
 }
 }
